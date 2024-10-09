fbb1a9689db7666dfa42a3d9416fbfb9c1099f10
duonghuupha <duonghuupha@hotmail.com.vn>
2024-10-09 15:07:44 +0700

    Code 1

---
+++ public/scripts/javascript/event.js
@@ -0,0 +1,203 @@
+/**
+ * 
+ * @param {*} id_form 
+ * @param {*} post_url 
+ * @param {*} url_reject 
+ */
+function save_reject(id_form, post_url, url_reject){
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData($(id_form)[0]);
+    $('.overlay').show();
+    $.ajax({
+        url: post_url,  //server script to process data
+        type: 'POST',
+        xhr: function() {
+            return xhr;
+        },
+        data: formData,
+        success: function(data){
+            var result = JSON.parse(data);
+            if(result.success == true){
+                window.location.href = url_reject;
+            }else{
+                $('.overlay').hide();
+                show_message('error', result.msg);
+                return false;
+            }
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+/**
+ * 
+ * @param {*} id_form 
+ * @param {*} post_url 
+ * @param {*} id_modal 
+ * @param {*} id_content 
+ * @param {*} url_refresh 
+ */
+function save_form_modal(id_form, post_url, id_modal, id_content, url_refresh){
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData($(id_form)[0]);
+    $('.overlay').show();
+    $.ajax({
+        url: post_url,  //server script to process data
+        type: 'POST',
+        xhr: function() {
+            return xhr;
+        },
+        data: formData,
+        success: function(data){
+            var result = JSON.parse(data);
+            if(result.success == true){
+                $('.overlay').hide();
+                show_message('success', result.msg);
+                $(id_modal).modal('hide');
+                //$(id_content).load(url_refresh);
+                $(id_content).trigger('reloadGrid');
+            }else{
+                $('.overlay').hide();
+                show_message('error', result.msg);
+                return false;
+            }
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+/**
+ * 
+ * @param {*} id_form 
+ * @param {*} post_url 
+ * @param {*} id_modal 
+ * @param {*} id_content 
+ * @param {*} url_refresh 
+ */
+function save_form_modal_old(id_form, post_url, id_modal, id_content, url_refresh){
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData($(id_form)[0]);
+    $('.overlay').show();
+    $.ajax({
+        url: post_url,  //server script to process data
+        type: 'POST',
+        xhr: function() {
+            return xhr;
+        },
+        data: formData,
+        success: function(data){
+            var result = JSON.parse(data);
+            if(result.success == true){
+                $('.overlay').hide();
+                show_message('success', result.msg);
+                $(id_modal).modal('hide');
+                $(id_content).load(url_refresh);
+            }else{
+                $('.overlay').hide();
+                show_message('error', result.msg);
+                return false;
+            }
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+/**
+ * 
+ * @param {*} id_form 
+ * @param {*} post_url 
+ * @param {*} id_content 
+ * @param {*} url_refresh 
+ */
+function save_form_line(id_form, post_url, id_content, url_refresh){
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData($(id_form)[0]);
+    $('.overlay').show();
+    $.ajax({
+        url: post_url,  //server script to process data
+        type: 'POST',
+        xhr: function() {
+            return xhr;
+        },
+        data: formData,
+        success: function(data){
+            var result = JSON.parse(data);
+            if(result.success == true){
+                $('.overlay').hide();
+                show_message('success', result.msg);
+                //$(id_content).load(url_refresh);
+                $(id_content).trigger('reloadGrid');
+            }else{
+                $('.overlay').hide();
+                show_message('error', result.msg);
+                return false;
+            }
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+/**
+ * 
+ * @param {*} str_data 
+ * @param {*} notify 
+ * @param {*} post_url 
+ * @param {*} id_div 
+ * @param {*} url_refresh 
+ */
+function del_data(str_data, notify, post_url, id_div, url_refresh){
+    bootbox.confirm({
+        message: notify,
+        buttons:{
+            confirm: {
+                label: "Đồng ý",
+                className: "btn-danger btn-sm"
+            },
+            cancel: {
+                label: "Không đồng ý",
+                className: "btn-primary btn-sm"
+            }
+        },
+        callback: function(result){
+            if(result){
+                exec_del(str_data, post_url, id_div, url_refresh);
+            }
+        }
+    });
+}
+
+/**
+ * 
+ * @param {*} data_str 
+ * @param {*} url_data 
+ * @param {*} id_div 
+ * @param {*} url_refresh 
+ */
+function exec_del(data_str, url_data, id_div, url_refresh){
+    $('.overlay').show();
+    $.ajax({
+        type: "POST",
+        url: url_data,
+        data: data_str, // serializes the form's elements.
+        success: function(data){
+            var result = JSON.parse(data);
+            if(result.success == true){
+                $('.overlay').hide();
+                show_message('success', result.msg);
+                $(id_div).trigger('reloadGrid');
+            }else{
+                $('.overlay').hide();
+                show_message('error', result.msg);
+                return false;
+            }
+        }
+    });
+}
---
+++ public/scripts/javascript/properties.js
@@ -0,0 +1,140 @@
+$(function(){
+    $('.select2').select2(); // Select box
+    $('.date-picker').datepicker({autoclose: true,todayHighlight: true}); // Datebox
+    //Filebox
+    $('.file_attach').ace_file_input({
+        no_file:'Không có file ...',btn_choose:'Lựa chọn',
+        btn_change:'Thay đổi',droppable:false,
+        onchange:null,thumbnail:true
+    });
+    $('.overlay').hide(); // loading
+    $("input[data-type='currency']").on({ // Format currency onkeypress input
+        keyup: function() {formatCurrency($(this));}
+    });
+    $("input[data_type='currency']").on({ // Format currency onkeypress input
+        keyup: function() {formatCurrency($(this));}
+    });
+    $('[data-rel=tooltip]').tooltip({container:'body'});
+    $('[data-rel=popover]').popover({container:'body'});
+});
+
+/**
+ * Display notifycation
+ * @param {*} icon 
+ * @param {*} msg 
+ */
+function show_message(icon, msg){
+    $.toast({
+        heading: 'Thông báo',
+        text: msg,
+        showHideTransition: 'fade',
+        icon: icon,
+        position: 'top-right'
+    })
+}
+
+/**
+ * Format number
+ * @param {*} n 
+ * @returns 
+ */
+function formatNumber(n) {
+    return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+
+function formatCurrency(input, blur) {
+    var input_val = input.val();
+    if (input_val === "") { return; }
+    var original_len = input_val.length;
+    var caret_pos = input.prop("selectionStart");
+    if (input_val.indexOf(".") >= 0) {
+        var decimal_pos = input_val.indexOf(".");
+        var left_side = input_val.substring(0, decimal_pos);
+        var right_side = input_val.substring(decimal_pos);
+        left_side = formatNumber(left_side);
+        right_side = formatNumber(right_side);
+        if (blur === "blur") {
+            right_side += "00";
+        }
+        right_side = right_side.substring(0, 2);
+        input_val = left_side + "." + right_side;
+    } else {
+        input_val = formatNumber(input_val);
+        input_val = input_val;
+        if (blur === "blur") {
+            input_val += ".00";
+        }
+    }
+    input.val(input_val);
+    var updated_len = input_val.length;
+    caret_pos = updated_len - original_len + caret_pos;
+    input[0].setSelectionRange(caret_pos, caret_pos);
+}
+
+function validate(evt) {
+    var theEvent = evt || window.event;
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+        // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
+
+function CurrencyFormatted(amount){
+	return amount.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+}
+
+function updatePagerIcons(table) {
+    var replacement = 
+    {
+        'ui-icon-seek-first' : 'ace-icon fa fa-angle-double-left bigger-140',
+        'ui-icon-seek-prev' : 'ace-icon fa fa-angle-left bigger-140',
+        'ui-icon-seek-next' : 'ace-icon fa fa-angle-right bigger-140',
+        'ui-icon-seek-end' : 'ace-icon fa fa-angle-double-right bigger-140'
+    };
+    $('.ui-pg-table:not(.navtable) > tbody > tr > .ui-pg-button > .ui-icon').each(function(){
+        var icon = $(this);
+        var $class = $.trim(icon.attr('class').replace('ui-icon', ''));
+        
+        if($class in replacement) icon.attr('class', 'ui-icon '+replacement[$class]);
+    })
+}
+
+function combo_select_2(id, url_data, selected, title_selected){
+    $(id).select2({
+        ajax: {
+            url: url_data,
+            dataType: 'json',
+            type: 'GET',
+            data: function (params) {
+                var queryParameters = {
+                    q: params.term
+                }
+                return queryParameters;
+            },
+            processResults: function (data) {
+                return {
+                    results: $.map(data, function (item) {
+                        return {
+                            text: item.title,
+                            id: item.id,
+                            disabled: item.disabled
+                        }
+                    })
+                };
+            }
+        }
+    });
+    if(selected != 0){
+        var $option = $('<option selected>'+title_selected+'</option>').val(selected);
+        $(id).append($option).trigger('change');
+    }
+}
